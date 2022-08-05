0cbb23d5bf8e8177b552d40c5efc39d405973b62
jinwei69 <ss610349@gmail.com>
2022-08-05 21:47:27 +0800

    ??

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddResponseCompression();
 
 var app = builder.Build();
 
@@ -16,14 +15,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseResponseCompression();
-
-app.UseCors(r => r.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
 
 app.UseHttpsRedirection();
 
