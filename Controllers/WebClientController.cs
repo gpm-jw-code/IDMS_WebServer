d8b905de49be552d0416f4ab7bc1796200f87feb
jinwei69 <ss610349@gmail.com>
2022-08-22 14:44:55 +0800

    Add Controllers/WebClientController.cs / Controllers/EdgeController.cs

---
+++ Controllers/WebClientController.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDMSWebServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebClientController : ControllerBase
+    {
+        private ILogger<WebClientController> logger;
+
+        public WebClientController(ILogger<WebClientController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet("/ModuleInfoGet")]
+        public async Task GetModuleInfo(string edgeIP)
+        {
+            IsWebsocketRequest(out bool iswebsocket, out System.Net.WebSockets.WebSocket client);
+            if (iswebsocket && client.State == System.Net.WebSockets.WebSocketState.Open)
+            {
+                Task.Factory.StartNew(async () => await client.ReceiveAsync(new ArraySegment<byte>(), CancellationToken.None));
+                while (client.State == System.Net.WebSockets.WebSocketState.Open)
+                {
+                    await Task.Delay(100);
+                    string stateJson = Models.IDMS.DataMiddleware.Fetch.GetModuleStateData(edgeIP);
+                    if (stateJson == null)
+                        continue;
+                    client.SendAsync(new ArraySegment<byte>(Encoding.ASCII.GetBytes(stateJson)),System.Net.WebSockets.WebSocketMessageType.Text,true,CancellationToken.None);
+
+                }
+            }
+        }
+
+        [HttpGet("/Dignose")]
+        public async Task GetDignoseData(string type, string edgeIP, string? sensorIP, string? chart_type)
+        {
+            IsWebsocketRequest(out bool iswebsocket, out System.Net.WebSockets.WebSocket client);
+            if (iswebsocket)
+            {
+                logger.LogInformation("Websocket Client Connected");
+
+                byte[] dataBuf = null;
+
+                Task.Factory.StartNew(async () => await client.ReceiveAsync(new ArraySegment<byte>(), CancellationToken.None));
+
+                while (client.State == System.Net.WebSockets.WebSocketState.Open)
+                {
+                    string dignoseDataJson = null;
+                    if (type == "list")
+                    {
+                        dignoseDataJson = Models.IDMS.DataMiddleware.Fetch.GetDignoseDataJson(edgeIP);
+                    }
+                    else
+                    {
+                        if (sensorIP == null | chart_type == null)
+                            break;
+
+                        if (chart_type == "HS")
+                            dignoseDataJson = Models.IDMS.DataMiddleware.Fetch.GetHSChartingDataJsonBySensorIP(edgeIP, sensorIP);
+                        if (chart_type == "AID")
+                            dignoseDataJson = Models.IDMS.DataMiddleware.Fetch.GetAIDChartingDataJsonBySensorIP(edgeIP, sensorIP);
+                        if (chart_type == "AIH")
+                            dignoseDataJson = Models.IDMS.DataMiddleware.Fetch.GetAIHChartingDataJsonBySensorIP(edgeIP, sensorIP);
+
+                    }
+
+                    if (dignoseDataJson != null)
+                    {
+                        dataBuf = Encoding.ASCII.GetBytes(dignoseDataJson);
+                    }
+                    await Task.Delay(1000);
+                    if (dataBuf == null)
+                        continue;
+                    try
+                    {
+                        await client.SendAsync(new ArraySegment<byte>(dataBuf), System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
+                        logger.LogInformation("Send Dignose Data to Websocket Client");
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError("Send Dignose Data to Websocket Client fail,{0}", ex.Message);
+
+                    }
+
+                }
+            }
+        }
+
+
+        private void IsWebsocketRequest(out bool isWebsocket, out System.Net.WebSockets.WebSocket client)
+        {
+            client = null;
+            isWebsocket = HttpContext.WebSockets.IsWebSocketRequest;
+            if (isWebsocket)
+                client = HttpContext.WebSockets.AcceptWebSocketAsync().Result;
+        }
+    }
+}
---
+++ Models/IDMS/IDMSEdgeData.cs
@@ -0,0 +1,11 @@
+﻿namespace IDMSWebServer.Models.IDMS
+{
+    public class IDMSEdgeData
+    {
+        public string DignoseData { get; set; }
+        public string HSCharingData { get; internal set; }
+        public string AIHCharingData { get; internal set; }
+        public string AIDCharingData { get; internal set; }
+        public string ModuleStatesData { get; internal set; }
+    }
+}
